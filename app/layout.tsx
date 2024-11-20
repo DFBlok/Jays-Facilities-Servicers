d4dba81357e42b530077d631418aef7d9acd24b6
DFBlok <s219392862@mandela.ac.za>
2024-11-20 14:15:50 +0200

    Footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "../components/footer";
 import { Navigation } from "../components/navigation";
+import { Footer } from "../components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
